8a20a6e70b925bca3dd40748fdd66cd8d0f9237c
Keith <kmson7@gmail.com>
2020-03-24 20:08:35 +0800

    added support for non-singular numbers

--- script.js
+++ script.js
@@ -59,40 +59,56 @@ function operate(wholeArr) {
 // DONE: change 'sum' to '+' and for other operators
 // DONE: display calc without commas
 // TODO: support for multiple digit numbers
+// TODO: make result the first and only item in array when equals is pressed
 
+
+let stagingArr = [];
 let displayArr = [];
-
-let numpadButtons = document.querySelectorAll('.numpad button');
-numpadButtons.forEach((button) => 
-    button.addEventListener('click', () => {
-        displayArr.push(parseInt(button.textContent));
-        changeDisplayText(displayArr.join(' '));
-    })
-);
-
-let operatorButtons = document.querySelectorAll('.operator button');
-operatorButtons.forEach((button) => 
-    button.addEventListener('click', () => {
-        displayArr.push(button.textContent);
-        changeDisplayText(displayArr.join(' '));
-    })
-);
 
 function changeDisplayText(displayText) {
     let display = document.querySelector('.display');
     display.textContent = displayText;
 }
 
+let numpadButtons = document.querySelectorAll('.numpad button');
+numpadButtons.forEach((button) => 
+    button.addEventListener('click', () => {
+        stagingArr.push(parseInt(button.textContent));
+        changeDisplayText(stagingArr.join(''));
+    })
+);
+
+let operatorButtons = document.querySelectorAll('.operator button');
+operatorButtons.forEach((button) => 
+    button.addEventListener('click', () => {
+
+        if (stagingArr == 0) {
+            // alert('staging area is empty')
+        } else {
+            // alert('pushing staged to display')
+            displayArr.push(parseInt(stagingArr.join('').toString()));
+        } 
+        stagingArr = [];
+        displayArr.push(button.textContent);
+        changeDisplayText(displayArr.join(' '));
+    })
+);
+
 let equals = document.querySelector('#equals');
-equals.addEventListener('click', () => changeDisplayText(operate(displayArr)));
+equals.addEventListener('click', () => {
+    if (stagingArr == 0) {
+        // alert('staging area is empty')
+    } else {
+        // alert('pushing staged to display')
+        displayArr.push(parseInt(stagingArr.join('').toString()));
+    } 
+    stagingArr = [];
+    changeDisplayText(operate(displayArr))
+});
 
 let clear = document.querySelector('#clear');
 clear.addEventListener('click', () => {
     displayArr = [];
+    stagingArr = [];
     changeDisplayText('0');
 })
-
-// let testArr = [3, 'divide', 5, 'subtract', 10 , 'add', 2, 'multiply', 7]
-
-// operate(testArr);
-
